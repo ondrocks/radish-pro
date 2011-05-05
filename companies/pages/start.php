0c64a1076cc48ae6f984e53d3d6ee04328416c9a
Pieter <pieter@nr8.nl>
2011-05-05 22:13:31 +0200

    adde controllerFront

--- companies/pages/start.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-?>
-<h1>Companies</h1>
--- config/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include '../api/globals.php';
-include '../api/loader.php';
-
-include 'header.php';
-include 'main.php';
-include 'footer.php';
-?>
