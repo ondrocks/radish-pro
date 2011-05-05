bb49864eb65637792c138e0fb4d539c2b809a59f
Pieter <pieter@nr8.nl>
2011-05-06 01:29:49 +0200

    added views

---
+++ view/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <meta name='description' content='Make a brand profile'/>
+	<meta name="viewport" content="height=device-height, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=yes"/>
+	<meta name='apple-mobile-web-app-capable' content='yes' />
+	<meta name='apple-mobile-web-app-status-bar-style' content='black' />
+	<base href='<?php echo PUtil::baseUrl();?>'/>
+        <title>radish-pro.com</title>
+        <link rel='stylesheet' href='styles.css' type='text/css'/>
+	<script type='text/javascript' src='../js/functions.js'></script>
+	<link href='https://fonts.googleapis.com/css?family=Lato:regular' rel='stylesheet' type='text/css'/>
+	<script type='text/javascript'>
+	djConfig = {
+ 		modulePaths: {
+			"dojo": "https://ajax.googleapis.com/ajax/libs/dojo/1.3.2/dojo",
+			"dijit": "https://ajax.googleapis.com/ajax/libs/dojo/1.3.2/dijit",
+			"dojox": "https://ajax.googleapis.com/ajax/libs/dojo/1.3.2/dojox"
+  		} 
+	}
+	</script>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/dojo/1.6/dojo/dojo.xd.js" type="text/javascript"></script> 
+
+</head>
+
