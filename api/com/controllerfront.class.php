114ed7517330ef6a0836bdeeaafea88c192b4bc1
Pieter <pieter@nr8.nl>
2011-05-06 01:17:40 +0200

    renamed templates/ into controller/

--- api/com/controllerfront.class.php
+++ api/com/controllerfront.class.php
@@ -4,9 +4,9 @@ class controllerFront
 	function __construct($controller='index')
 	{
 		isset($_GET['controller']) ? $controller = $_GET['controller'] : '';
-		include 'templates/header.php';
-		include 'templates/' . $controller . '/main.php';
-		include 'templates/' . $controller . '/footer.php';
+		include 'controller/header.php';
+		include 'controller/' . $controller . '/main.php';
+		include 'controller/' . $controller . '/footer.php';
 	}
 	function getController()
 	{
---
+++ controller/index/main.php
@@ -0,0 +1,39 @@
+<?php
+
+//include 'header.php';
+
+?>
+	<script type="text/javascript" src="http://www.google.com/jsapi"></script>
+
+	<script type='text/javascript'>
+	google.load("gdata", "2.x") ;
+	var contactsService = nul
+	</script>
+
+    <div id="fb-root"></div>
+
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({appId: '201316349891020', status: true, cookie: true,
+                 xfbml: true});
+       FB.Event.subscribe('auth.sessionChange', function(response) {
+    		if (response.session) {
+			document.location.href = '/login/facebook_login.php';
+      // A user has logged in, and a new cookie has been saved
+    		} else {
+      // The user has logged out, and the cookie has been cleared
+       		}
+       })
+      };
+      (function() {
+        var e = document.createElement('script');
+        e.type = 'text/javascript';
+        e.src = document.location.protocol +
+          '//connect.facebook.net/nl_NL/all.js';
+        e.async = true;
+        //document.getElementById('fb-root').appendChild(e);
+      }());
+    </script>
+
+login w/ LinkedIn : <a href='/login/linkedin_login.php'><img class='button' src='../logo_linkedin.png'/></a>
+<!--<fb:login-button></fb:login-button>-->
