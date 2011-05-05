e55b9993dabbc55e29d0b1f33d9ee7e398c10f7d
Pieter <pieter@nr8.nl>
2011-05-05 23:24:35 +0200

    fix controllerFront

--- api/loader.php
+++ api/loader.php
@@ -6,12 +6,8 @@ function __autoload($name)
 {
 	if(is_file(PATH_TO_COM . strtolower($name) . '.class.php'))
 		require_once(PATH_TO_COM . strtolower($name) . '.class.php');
-	else if(is_file('../admin/' . PATH_TO_COM . strtolower($name) . '.class.php'))
-		require_once('../admin/' . PATH_TO_COM . strtolower($name) . '.class.php');
 	else if(is_file('../api/' . PATH_TO_COM . strtolower($name) . '.class.php')) 
 		require_once('../api/' . PATH_TO_COM . strtolower($name) . '.class.php');
-	else
-		require_once(PATH_TO_COM . strtolower($name). '.class.php');
 }
 
 $globalIniArray = parse_ini_file( PUtil::docRoot() . "en-GB.language.ini");
--- search/index.php
+++ search/index.php
@@ -4,8 +4,6 @@ include '../api/globals.php';
 include '../api/loader.php';
 include '../api/credentials.php';
 
-include '../login/linkedin.php';
-
 	session_start();
 
 if(isset($_POST['q']) && isset($_POST['select']))
