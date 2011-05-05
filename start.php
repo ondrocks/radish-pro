a33aad405429d2b384b5f2ff58118546de8f4f7d
Pieter <pieter@nr8.nl>
2011-05-06 00:16:48 +0200

    minor changes

--- start.php
+++ start.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once 'api/credentials.php';
-include_once "login/linkedin.php";
 include 'api/globals.php';
 
 function __autoload($name){
@@ -10,8 +9,5 @@ function __autoload($name){
 
 $globalIniArray = parse_ini_file( PUtil::docRoot() . "en-GB.language.ini");
 
-if(isset($_GET['controlller']))
-	new controllerFront($_GET['controller']);
-else
-	new controllerFront('index');
+new controllerFront();
 ?>
