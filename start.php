b8c1dae1dab6270fbd5b104936ae0a06365ddf87
Pieter <pieter@nr8.nl>
2011-05-14 01:37:28 +0200

    api now controller

--- start.php
+++ start.php
@@ -4,10 +4,6 @@ require_once 'conf/credentials.php';
 include 'conf/globals.php';
 include 'conf/loader.php';
 
-//function __autoload($name){
-//	require_once('api/com/'.strtolower($name).'.class.php');
-//}
-
 $globalIniArray = parse_ini_file( PUtil::docRoot() . "/language/en-GB.language.ini");
 
 new controllerFront();
