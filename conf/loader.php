468366d9b9d688b1a97f3608aa7531d0d2280a20
Pieter <pieter@nr8.nl>
2011-05-14 02:10:08 +0200

    added com/ folder in root

--- conf/loader.php
+++ conf/loader.php
@@ -1,13 +1,11 @@
 <?php
 
-define('PATH_TO_COM', 'com/');
+define('PATH_TO_COM', 'com' . DS);
 
 function __autoload($name)
 {
 	if(is_file(PATH_TO_COM . strtolower($name) . '.class.php'))
 		require_once(PATH_TO_COM . strtolower($name) . '.class.php');
-	else if(is_file('controller/api/' . PATH_TO_COM . strtolower($name) . '.class.php')) 
-		require_once('controller/api/' . PATH_TO_COM . strtolower($name) . '.class.php');
 	else if(is_file('controller/' . controllerFront::getController() . DS . PATH_TO_COM . strtolower($name) . '.class.php'))
 		require_once('controller/' . controllerFront::getController() . DS . PATH_TO_COM . strtolower($name) . '.class.php');
 }
