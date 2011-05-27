246c69dc4945dafc75a8f1d2ef9b6b4d59368401
Pieter <pieter@nr8.nl>
2011-05-27 03:00:18 +0200

    removed lines for import connections

--- controller/api/com/apicommand.class.php
+++ controller/api/com/apicommand.class.php
@@ -8,9 +8,11 @@ class APICommand
 	var $cacheable = false;
 	var $lastId = 0;
 	var $dtb;
+	var $linkedIn;
 
 	function __construct($command, $query, $level, $cacheable)
 	{
+		$this->linkedIn = $this->initLinkedInApp();
 		$this->dtb = new Dtb();
 		$this->command = $command;
 		$this->query = $query;
@@ -33,68 +35,6 @@ class APICommand
 		$linkedin->oauth_verifier   =   $_SESSION['oauth_verifier'];
 		$linkedin->access_token     =   unserialize($_SESSION['oauth_access_token']);
 		return $linkedin;
-	}
-
-	private function saveLinkedInConnection($linkedin, $firstName, $lastName,  $user, $headline,$place, $country, $pictureUrl, $profileUrl, $company )
-	{
-		$query = "insert into " . TABLE_PREFIX . "linkedin_connections (linkedin) values (:linkedin)";
-		$values = array("linkedin" => $linkedin);
-		$this->dtb->prepareAndExecute($query, $values);
-		if($this->dtb->dtb->lastInsertId())
-		{
-			$query = "insert into " . TABLE_PREFIX . "people (firstName, lastName, user, place, country, linkedIn, headline, ip, pictureUrl, profileUrl)
-				values(:firstName, :lastName, :user, :place, :country, :linkedIn, :headline, :ip, :pictureUrl, :profileUrl)";
-			$values = array('firstName' => $firstName,
-					'lastName' => $lastName,
-					'user' => $user,
-					'place' => $place,
-					'country' => $country,
-					'linkedIn' => $this->dtb->dtb->lastInsertId(),
-					'headline' => $headline,
-					'ip' => $_SERVER['REMOTE_ADDR'],
-					'pictureUrl' => $pictureUrl,
-					'profileUrl' => $profileUrl);
-			$this->dtb->prepareAndExecute($query, $values);
-
-			if($this->dtb->dtb->lastInsertId())
-			{
-				$peopleId = $this->dtb->dtb->lastInsertId();
-				$query = "select count(*) as count, id from #_companies where name = :name";
-				$values = array('name' => $company['name']);
-				$this->dtb->prepareAndExecute($query, $values);
-				$result = $this->dtb->getAllRows(false);
-				if($result[0]->count == 0)
-				{
-					$query = "insert into #_companies(name, size, ticker, industry) 
-						values(:name, :size, :ticker, :industry)";
-					$values = array(
-						'name' => $company['name'],
-						'industry' => $company['industry'],
-						'size' => $company['size'],
-						'ticker' => $company['ticker']);
-					$this->dtb->prepareAndExecute($query, $values);
-					$companyId = $this->dtb->dtb->lastInsertId();
-				}
-				else if(isset($result[0]->id))
-				{
-					$companyId = $result[0]->id;
-				}
-				else 
-					$companyId =0;
-				$query = "select count(*) as count from #_positions where company_id = :companyId and people_id = :peopleId";
-				$values = array('companyId' => $companyId, 
-						'peopleId' => $peopleId);
-				$this->dtb->prepareAndExecute($query, $values);
-				$result = $this->dtb->getAllRows(false);
-				if($result[0]->count == 0)
-				{
-					$query = "insert into #_positions(company_id, people_id) values(:companyId, :peopleId)";
-					$values = array('companyId' => $companyId,
-							'peopleId' => $peopleId);
-					$this->dtb->prepareAndExecute($query, $values);
-				}
-			}
-		}
 	}
 
 	private function saveEmailConnection($name, $email)
@@ -215,38 +155,6 @@ class APICommand
 					$address['foundName'] = '';
 				}
 				echo json_encode($address);
-			}
-		}
-		else if($this->command == 'import_connections')
-		{
-			$linkedin = $this->initLinkedInApp();	
-			if($linkedin)
-			{
-				$data = $linkedin->getConnections();
-				$xml = simplexml_load_string($data);
-				foreach($xml->person as $person)
-				{
-					if((string)$person->id == 'private')
-						continue;
-					$firstName = (string)$person->{'first-name'};
-					$lastName = (string)$person->{'last-name'};
-					$linkedinid = (string)$person->id;
-					$headline = $person->headline;
-					$_user = (int)$user->getId();
-					$place = (string)$person->location->name;;
-					$country = (string)$person->location->country->code;
-					$address = (string)$person->{'main-address'};
-					$profileUrl = (string)$person->{'public-profile-url'};
-					$pictureUrl = (string)$person->{'picture-url'};
-					$companyId = $person->{'positions'};
-					$company = array(
-						"name" => $companyId->position[0]->company->name,
-						"industry" => $companyId->position[0]->company->industry,
-						"size" => preg_replace('/^(.*)(employees)$/', '$1', $companyId->position[0]->company->size),
-						"ticker" => $companyId->position[0]->company->ticker);		
-
-					$this->saveLinkedInConnection($linkedinid, $firstName, $lastName, $_user, $headline, $place, $country, $pictureUrl, $profileUrl, $company);
-				}
 			}
 		}
 		else if($this->command == 'list_emails')
